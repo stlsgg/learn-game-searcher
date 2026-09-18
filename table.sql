6b70a58be75be2d896902920933fbaeb878f145a
stlsgg <gregory.guzey@gmail.com>
2026-09-18 21:48:53 +0300

    add sql scripts for project

---
+++ table.sql
@@ -0,0 +1,10 @@
+CREATE TYPE schedule_type AS ENUM ('lecture', 'practice');
+
+CREATE TABLE IF NOT EXISTS schedule (
+    id SERIAL PRIMARY KEY,
+    subject_name VARCHAR(255) NOT NULL,
+    starting_at TIMESTAMP NOT NULL,
+    teacher VARCHAR(255) NOT NULL,
+    cabinet INT NOT NULL,
+    type schedule_type NOT NULL
+);
