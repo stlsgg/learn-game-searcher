ae1def1ac02a5761816c1139bd0d4dc08922f377
stlsgg <gregory.guzey@gmail.com>
2026-09-20 08:03:58 +0300

    add todos and route stubs in main.py

--- main.py
+++ main.py
@@ -16,6 +16,65 @@ async def healthcheck():
     return { "status": "ok" }
 
 
+@app.get("/game")
+async def get_game():
+    """retrieve games from library."""
+    # TODO route accept query filters: /game?query_filters
+    # TODO add query filters: limit, offset, genre, category, release_before,
+    # release_after, price
+    # TODO add /game/{id} for retrieving specific game from library by its id
+    # TODO add /game/{id}?query_filters with basic filters:
+    # - release_before and release_after: takes dd.mm.yyyy
+    # - genre, category: takes string
+    # - price: float with two positions in manticca (10, 2)
+    # TODO find out: can we write multiple times to search query same field or
+    # not, like https://foo.bar/?genre=roguelike&genre=smasher to combine
+    # filters together?
+    return {}
+
+
+@app.post("/game/{id}")
+async def change_game_info():
+    """full or partial game information change."""
+    # TODO return 200, object: new game
+    # TODO fields which can be modified:
+    # name, description, released_at, genre, categories, price,
+    # hardware_requirements; if passed other fields - reject with 400.
+    return {}
+
+
+@app.post("/game")
+async def add_new_game():
+    """create new game and add it to library."""
+    # TODO return custom code 201 on successful creation
+    return {}
+
+
+@app.delete("/game/{id}")
+async def delete_game():
+    """remove game from library completely."""
+    # TODO custom http code 204 'no content' without return body
+    return {}
+
+
+@app.post("/game/{id}/embed")
+async def create_embedding():
+    """process game info into vectors and save it to game data."""
+    # TODO return immediately to user, return 202 accepted
+    # NOTE: process must go away from fastapi and should be handled in the
+    # background
+    return {}
+
+# general TODO:
+# TODO custom error handler for all wrong dto:
+# { "status": "error", "message": "bad request" }
+# TODO keep returning object shapes the same:
+# { "status": "error|ok", "message": "string|{object with fields}"}
+# TODO rate limiting with floating window 2 minutes, 25 requests max. for all
+# routes - one user cannot make >25 requests per 2 minutes. for learning purpose
+# only.
+# NOTE: if you have idea what to add in routes, youre welcome to tell about it.
+
 @app.post("/ask")
 async def ask_ai(question: Question):
     """ask ai agent question, related to the app data."""
